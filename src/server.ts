3c812ae1e5c170ffc24fb4976706ad938d22ad21
swedensnow <abuzeid2222@gmail.com>
2023-04-21 15:29:40 +0200

    middleware
    - :package: [cors, morgan, express.urlencoded]

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,24 @@
 import express from 'express';
 import router from './router';
+import morgan from 'morgan';
 
 const app = express();
+
+const customLogger = (message) => (req, res, next) => {
+	console.log(`Hello from ${message}`);
+	next();
+}
+
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(customLogger('custom logger'));
+
+// app.use((req, res, next) => {
+// 	res.status(401)
+// 	res.send('Nope!')
+
+// })
 
 app.get('/', (req, res) => {
 console.log('helllo from express');
