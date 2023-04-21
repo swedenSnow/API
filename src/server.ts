cb32a446c7ffc57f48fea7be9bdd8a4011cbf510
swedensnow <abuzeid2222@gmail.com>
2023-04-21 14:50:07 +0200

    router
    - Added router [api/]

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import router from './router';
 
 const app = express();
 
@@ -7,5 +8,7 @@ console.log('helllo from express');
 	res.status(200)
   	res.send('Hello World!');
 });
+
+app.use('/api', router);
 
 export default app;
