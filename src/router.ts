d5cf3e1beaa986a27831b667db5bb4e68fb4a33f
swedensnow <abuzeid2222@gmail.com>
2023-04-25 13:03:43 +0200

    validation
    - :package: [express-validator]
    - :file_folder: middleware.ts
    - router update

--- src/router.ts
+++ src/router.ts
@@ -1,4 +1,8 @@
 import { Router } from 'express';
+import { body, validationResult, oneOf } from 'express-validator';
+import { createProduct, deleteProduct, getOneProduct, getProducts, updateProduct } from './handlers/product';
+import { createUpdate, deleteUpdate, getOneUpdate, getUpdates, updateUpdate } from './handlers/update';
+import { handleInputsErrors } from './modules/middleware';
 
 const router = Router();
 
@@ -6,34 +10,65 @@ const router = Router();
  * Product
  */
 
-router.get('/product', (req, res) => {
-	res.json({
-        message: 'hello',
-    });
-})
-router.get('/product/:id', () => {})
-router.put('/product/:id', () => {})
-router.post('/product', () => {})
-router.delete('/product/:id', () => {})
+router.get('/product', getProducts);
+router.get('/product/:id', getOneProduct);
+router.put(
+    '/product/:id',
+    body('name').isString(),
+    handleInputsErrors,
+    updateProduct
+);
+router.post(
+    '/product',
+    body('name').isString(),
+    handleInputsErrors,
+    createProduct
+);
+router.delete('/product/:id', deleteProduct);
 
 /**
  * update
  */
 
-router.get('/update', () => {})
-router.get('/update/:id', () => {})
-router.put('/update/:id', () => {})
-router.post('/update', () => {})
-router.delete('/update/:id', () => {})
+router.get('/update', getUpdates);
+router.get('/update/:id', getOneUpdate);
+router.put(
+    '/update/:id',
+    body('title').optional(),
+    body('body').optional(),
+    body('status').isIn(['IN_PROGRESS', 'SHIPPED', 'DEPRECATED']).optional(),
+    body('version').optional(),
+    updateUpdate
+);
+
+router.post(
+    '/update',
+    body('title').exists().isString(),
+    body('body').exists().isString(),
+    body('productId').exists().isString(),
+    createUpdate 
+);
+router.delete('/update/:id', deleteUpdate);
 
 /**
  * Update point
  */
 
-router.get('/updatepoint', () => {})
-router.get('/updatepoint/:id', () => {})
-router.put('/updatepoint/:id', () => {})
-router.post('/updatepoint', () => {})
-router.delete('/updatepoint/:id', () => {})
+router.get('/updatepoint', () => {});
+router.get('/updatepoint/:id', () => {});
+router.put(
+    '/updatepoint/:id',
+    body('name').optional().isString(),
+    body('description').optional().isString(),
+    () => {}
+);
+router.post(
+    '/updatepoint',
+    body('name').isString(),
+    body('description').isString(),
+    body('updateId').exists().isString(),
+    () => {}
+);
+router.delete('/updatepoint/:id', () => {});
 
 export default router;
